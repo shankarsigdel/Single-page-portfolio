e494e36b1553d499f3cb8ff36ba1f769990881b6
Shankar <shankarsigdel26@gmail.com>
2021-05-07 20:47:42 -0400

    first commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,74 +1,94 @@
-// setup your IIFE (Immediately Invoked Function Expression)
-/*    JavaScript 
-      Mid-Term Exam
-      Author: Shankar Sigdel
-      Student ID: 301110925
-      app.js a Travel Report
-      Date:   2020-07-09
-*/
-
 (function () {
 "use strict";
 
-
-// YOUR CODE GOES HERE
-
 //Main Content Section injecting paragraphs
-document.getElementById("IntroParagraph").innerHTML=`Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals. 
-Greece has 1,400 islands, though only 230 of them are inhabited. 
-And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.`;
+document.getElementById("personalInformation").innerHTML=
+   `
+   <ul> Name: Shankar Sigdel </ul>
+   <ul> Address: 10 Coronation Drive, Scarborough, ON M1E 2H1 </ul>
+   <ul> Phone: +1 (647)608 7541 </ul>
+   <ul> Email: shankarsigdel26@gmail.com </ul>
+   `;
 
-document.getElementById("Folegandros").innerHTML="Folegandros is almost a Greek cliché, full of beautiful whitewashed houses with bright blue doors lining cobblestoned streets on steep cliffs. Rugged and remote, without chain hotels or package tours — not even a bank or ATM — and accessible only by ferry, the volcanic island boasts solitude in spades, secluded beaches, and incredible sunsets.";
+document.getElementById("missionstatement").innerHTML=
+   `
+   Gain Knowledge on Technology and Work as A Leading Professinal with Sound Knowledge.
+   `;
 
-document.getElementById("Alonissos").innerHTML="Off the mainland, this island, whose surrounding waters are a designated marine park full of seals and dolphins, is a nature lover's dream. The spectacular spot's crystal clear waters and beautiful beaches are perfect for diving, and its lush flora and dense forests ideal for hikes. Since the rich landscape is chock full of indigenous herbs, it's also known for its traditional medicine, and home of the International Academy of Homeopathy.";
+document.getElementById("professionalsummary").innerHTML=
+    `
+    <ul> Completed Software Engineering Technician, Diploma program </ul>
+    <ul> Completed Humanities and Social Sciences, Major English, University Degree Program </ul>
+    <ul> Love to work in culturally diverse environment </ul>
+    <ul> Skills in MS Word, Visio, Excel, PowerPoint, SPSS, Trello, GitHub </ul>
+    <ul> Good communication, Report writing and Documentation Skills </ul>
+    <ul> Conducted several research activities and prepared reports </ul>
+    `;
 
-document.getElementById("Spetses").innerHTML="This bohemian paradise is more Côte d'Azur than Grecian island. Spetses was hot in the '60s, when it hosted Marilyn Monroe and Elizabeth Taylor, and remains a swanky summer retreat for Athens' glitterati to this day, full of fancy hotels, mansions and yachts, and stylish boutiques. Aside from a handful of locally-owned cabs, the four-mile-long island has banned cars in town, giving it a refreshingly 19th-century vibe, with horse-drawn carriages being a common sight.";
+document.getElementById("relevantCourses").innerHTML=
+    `
+    <h5>Relevant Courses</h5>
+    <ul> Research Methods in Sociology and Anthropology </ul>
+    <ul> Critical and creative thinking </ul>
+    <ul> Literary Genre  </ul>
+    <ul> Research Analysis </ul>
+    `;
 
-document.getElementById("Amorgos").innerHTML="Shipwrecks, sea caves and beautiful clear waters make Amorgos popular with snorkelers, but the mountainous island is the perfect compromise between unwinding and adventure, boasting beautiful beaches but also hiking, scuba diving and rock-climbing. One of the island's main attractions is the 11th-century cliffside monastery of Panayia Hozoviótissa, which is precariously carved into a cliff.";
+    document.getElementById("releventCourses1").innerHTML=
+    `
+    <h5>Relevant Courses</h5>
+    <ul> C# Programming </ul>
+    <ul> Java Programming </ul>
+    <ul> UNIX/LINUX </ul>
+    <ul> Database </ul>
+    <ul> Web Application Devlopment </ul>
+    <ul> Client Side Web Devlopment </ul>
+    <ul> Software Development Project-I </ul>
+    <ul> Software Testing and Quality Assurance </ul>
+    <ul> Software Testing Automated Tools </ul>
+    <ul> IT Project Management </ul>
+    `;
 
-document.getElementById("Syros").innerHTML="A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners. Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways.";
+    document.getElementById("globalCollection").innerHTML=
+    `
+    <h5>Responsibilities</h5>
+    <ul> Responded over 40 calls per day </ul>
+    <ul> Solved the problems related to credit and collection department </ul>
+    <ul> Documented problems and reasons of the customers </ul>
+    <ul> Provided solutions to the customers </ul>
+    `;
 
-document.getElementById("Milos").innerHTML="This volcanic island, with catacombs and ancient iron mines to explore, is dramatically rugged, and famous for its stunning rock formations. Often likened to a moonscape, it's also known for its hot springs, the ancient Venus De Milo statue that was found here, and for its diversity of incredible beaches. Known as \"the island of colours\" it's home to around 80 beaches — some only accessible by boat — ranging from stunningly white, to striking black, and even unusual red and grey. However, each beach has the same Evian-clear turquoise waters, and is surrounded by a rugged mountain landscape.";
+    document.getElementById("tiffanyVillage").innerHTML=
+    `
+    <h5> Responsibilities</h5>
+    <ul> Served food to the elderly people </ul>
+    <ul> Followed Health and safety regulations </ul>
+    <ul> Assisted the F&B Manager and performed paper works as required. </ul>
+    <ul> Provided Immidiate solutions to the Residents </ul>
+           
+    `;
 
-document.getElementById("Hydra").innerHTML="The first thing you'll notice on this beautiful island is the lack of cars — and buses, motorcycles, or other wheeled vehicles, as well as high rises. The winding little streets full of beautiful 18th-century mansions, churches, cathedrals, museums and art galleries are covered in cobblestones, and mostly trafficked by donkeys and humans. Back in the day, the island attracted celebrities like Leonard Cohen and Sophia Loren, but was somehow forgotten over the years. That means that travellers to Hydra can have the chic yet almost rural island paradise more or less to themselves.";
+    document.getElementById("jbcCampus").innerHTML=
+    `
+    <h5>Responsibilities</h5>
+    <ul> Worked under the criteria and benchmarks of UGC </ul>
+    <ul> Published up to date information to the students and faculties </ul>
+    <ul> Organized the data and analyzed the data and Prepared SSR </ul>
+    <ul> Conducted Peer Review </ul>
+    <ul> Wrote proposals and applied for grants </ul>
+    <ul> Worked under the UGC guidelines on QAA </ul>
+    <ul> Developed proposals </ul>
+    <ul> Prepared Annual Reports </ul>
+    <ul> Develop procurement plan </ul>
+    `;
 
-document.getElementById("Ithaca").innerHTML="Most famous for being the home of Odysseus, and a prominent figure in Homer's \"The Odyssey,\" Ithaca is said to have been inhabited since the 2nd millennium BC. Made up of two islands joined by a narrow strip of land, many visit it to see the legendary sites mentioned by Homer. However, Ithaca is more than its mythical counterpart, beckoning with secluded beaches, dramatic cliffs, beautiful olive groves, and sleepy fishing villages full of Byzantine churches and monasteries.";
-
-document.getElementById("Gavdos").innerHTML="Near the more well-known Crete, Gavdos is the most southern island in Greece — and the most southern spot in Europe discounting the Canaries. Only accessible by ferry, the remote island has only about 50 permanent residents, and can feel like your private playground. Local legend has it that the island was the home of goddess Calypso, who kept Odysseus prisoner here. Today, a favorite tourist activity is to visit the spot believed to be her cave. Be warned, you won't find any luxury hotels here. While the romantically under-developed, super laid back island has a number of rooms for rent, those are limited, as the real draw here for many is the free, seaside camping.";
-
-//Contact Section form data stored in variables
-      //First Name
-      let fitstName=document.getElementById('firstName');
-      document.getElementById('contactForm').addEventListener('submit', function (e){
-      e.preventDefault();
-      console.log("App Started...")
-      console.log("Hi, My First Name is: ", firstName.value);
-      
-});
-      //Last Name
-      let lastName=document.getElementById('lastName');
-      document.getElementById('contactForm').addEventListener('submit', function (e){
-      e.preventDefault();
-      console.log("My Last Name is: ",lastName.value);
-});     
-      //Contact Number
-      let contactNumber=document.getElementById('contactNumber');
-      document.getElementById('contactForm').addEventListener('submit', function (e){
-      e.preventDefault();
-      console.log("My Contact Number is: ",contactNumber.value); 
-});     
-      // Email
-      let email=document.getElementById('email');
-      document.getElementById('contactForm').addEventListener('submit', function (e){
-      e.preventDefault();
-      console.log("My Email Address is: ",email.value);
-});
-      // Short Message Here
-      let shortMessage=document.getElementById('yourMessage');
-      document.getElementById('contactForm').addEventListener('submit', function (e){
-      e.preventDefault();
-      console.log("My Additional Information is: ",shortMessage.value);
-});
-      
+    document.getElementById("myInfo").innerHTML=
+    `
+    <h1 >Contact Me</h1>
+    <p>Name : Shankar Sigdel</p>
+    <p>Email: ssigdel2@my.centennialcollege.ca</p>
+    <p>Phone: (647) 608 7541</p>
+    <p>Address: Scarborough, ON, CA</p>
+    `;
 
 })();
